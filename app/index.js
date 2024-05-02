93b8e44cf82f8ed30ff9cb4c79c00bfcdd353047
AlexSichitiu <asichitiu@gmail.com>
2024-05-02 16:15:16 -0700

    Commented out the example Prisma queries.

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-prisma.user.create({
-    data: {
-        name: 'Alex',
-        email: 'test'
-    }
-})
-    .catch(_err => {})
-    .then(_res => {return prisma.user.findMany()})
-    .then(console.log)
+// Example Prisma queries.
+// prisma.user.create({
+//     data: {
+//         name: 'Alex',
+//         email: 'test'
+//     }
+// })
+//     .catch(_err => {})
+//     .then(_res => {return prisma.user.findMany()})
+//     .then(console.log)
