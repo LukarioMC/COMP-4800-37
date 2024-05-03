95ec4575f63e5c0efe421a68f1f57c49acc2d13b
Dakaro Mueller <LukarioMC@gmail.com>
2024-05-03 15:59:39 -0700

    Re-commented out example prisma query

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
-const { PrismaClient } = require('@prisma/client') 
+const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // Example Prisma queries.
-prisma.user.create({
-    data: {
-        id: 'aaa0037',
-        email: 'test',
-        hashed_password: 'test'
-    }
-})
-    .catch(console.log)
-    .then(_res => {return prisma.user.findMany()})
-    .then(console.log)
+// prisma.user.create({
+//     data: {
+//         id: 'aaa0037',
+//         email: 'test',
+//         hashed_password: 'test'
+//     }
+// })
+//     .catch(console.log)
+//     .then(_res => {return prisma.user.findMany()})
+//     .then(console.log)
