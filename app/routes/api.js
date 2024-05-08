21a581207cd43a6f3a4f8249242f7e4bb28ad583
AlexSichitiu <asichitiu@gmail.com>
2024-05-08 16:48:14 -0700

    Initialized files.

---
+++ app/routes/api.js
@@ -0,0 +1,12 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const express = require('express')
+const router = express.Router()
+
+router.get('api/fact', (req, res, next) => {
+
+})
+
+router.get('api/fact/:id', (req, res, next) => {
+
+})
