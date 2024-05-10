adc6b8c3df0b768da7c07fc2fcd7c68f83b9d4f9
AlexSichitiu <asichitiu@gmail.com>
2024-05-09 18:23:47 -0700

    Changed query format.

--- app/routes/api.js
+++ app/routes/api.js
@@ -61,12 +61,14 @@ function getFactByID(factID) {
             note,
             group_concat(name) as taglist
         FROM (
-            SELECT fulltag.id as cat_id, * FROM
-            factoid LEFT JOIN (
-                tag JOIN category
-                ON tag.category_id = category.id
-            ) as fulltag
-            ON factoid.id = factoid_id
+            SELECT 
+                fulltag.id as cat_id, *
+            FROM
+                factoid LEFT JOIN (
+                    tag JOIN category
+                    ON tag.category_id = category.id
+                ) as fulltag
+                ON factoid.id = factoid_id
         ) as fullfactoid
         WHERE is_approved AND id = ?
         GROUP BY id         
