c8dae4fba64585f53e0fdee538e6d3cde37785c0
AlexSichitiu <asichitiu@gmail.com>
2024-05-10 08:53:47 -0700

    Changed fact API to reflect the new fact/:id API.

--- app/routes/api.js
+++ app/routes/api.js
@@ -12,7 +12,7 @@ router.get('/api/fact', (req, res, next) => {
     try {
         let facts = getFacts(req.query.tag)
         let publicFieldFacts = facts.map(fact => {
-            let { is_approved, approval_date, cat_id, ...publicFields } = fact
+            let { is_approved, approval_date, ...publicFields } = fact
             return publicFields
         })
         return res.status(200).send(publicFieldFacts)
@@ -77,49 +77,56 @@ function getFactByID(factID) {
 /**
  * Given a list of tags, returns facts filtering out those who do not have all the given tags.
  * @param {*} tags a list of tag strings
- * @returns a list of facts (with taglist) whose tags are a superset of the input tags 
+ * @returns a list of facts (with taglist) whose tags are a superset of the input tags. Returns empty list if error occurs.
  */
-
 function getFacts(tags = undefined) {
-    let getFactsStmt = db.prepare(`
-    SELECT 
-        id, 
-        submitter_id,
-        content,
-        posting_date,
-        discovery_date,
-        note,
-        group_concat(name) as taglist
-    FROM (
+    try {
+        let getFactsStmt = db.prepare(`
         SELECT 
-            fulltag.id as cat_id, * 
-        FROM
-            factoid LEFT JOIN 
-                (tag JOIN category
-                ON tag.category_id = category.id
-                ) as fulltag
-            ON factoid.id = factoid_id
-    ) as fullfactoid
-    WHERE is_approved
-    GROUP BY id         
-    `)
-    let rawFacts = getFactsStmt.all()
-    rawFacts.forEach(fact => {
-        fact.taglist = fact.taglist ? fact.taglist.split(',').sort() : []
-    })
-
-    let facts = rawFacts
-    if (tags) {
-        tags = !Array.isArray(tags) ?  [tags] : tags
-        facts = rawFacts.filter(fact => {
-            return tags.every(tag => {
-                return fact.taglist.includes(tag)
-            })
+            id, 
+            group_concat(name) as taglist
+        FROM (
+            SELECT 
+                fulltag.id as cat_id, * 
+            FROM
+                factoid LEFT JOIN 
+                    (tag JOIN category
+                    ON tag.category_id = category.id
+                    ) as fulltag
+                ON factoid.id = factoid_id
+        )
+        WHERE is_approved
+        GROUP BY id         
+        `)
+        let unfilteredFacts = getFactsStmt.all()
+        unfilteredFacts.forEach(fact => {
+            fact.taglist = fact.taglist ? fact.taglist.split(',').sort() : []
         })
-    }
     
-    return facts
+        let filteredFacts = filterFacts(unfilteredFacts, tags)
+
+        return filteredFacts.map(fact => {
+            return getFactByID(fact.id)
+        })
+    } catch (e) {
+        console.log(e)
+        return []
+    }
 }
 
+/**
+ * Filters a list of facts by their tags. Returned facts have tags that are a superset of the given tags.
+ * @param {*} facts a list of facts to be filtered.
+ * @param {*} tags a list of tags to filter with.
+ * @returns a filtered list of facts.
+ */
+function filterFacts(facts, tags = []) {
+    tags = !Array.isArray(tags) ? [tags] : tags
+    return facts.filter(fact => {
+        return tags.every(tag => {
+            return fact.taglist.includes(tag)
+        })
+    })
+}
 
 module.exports = { router, getFactByID, getFacts }
--- db/test.js
+++ db/test.js
@@ -1,17 +1,43 @@
+const { getFactByID } = require('../app/routes/api')
+
 const db = require('better-sqlite3')('app.db')
 
-const all = db.prepare(`
-SELECT 
-    *, group_concat(name) as taglist
-FROM (
-    SELECT fulltag.id as cat_id, * FROM
-    factoid LEFT JOIN (
-        tag JOIN category
-        ON tag.category_id = category.id
-    ) as fulltag
-    ON factoid.id = factoid_id
-) as fullfactoid
-WHERE is_approved
-GROUP BY id         
-`)
-console.log(all.all())
+function getFacts(tags = undefined) {
+    let getFactsStmt = db.prepare(`
+    SELECT 
+        id, 
+        group_concat(name) as taglist
+    FROM (
+        SELECT 
+            fulltag.id as cat_id, * 
+        FROM
+            factoid LEFT JOIN 
+                (tag JOIN category
+                ON tag.category_id = category.id
+                ) as fulltag
+            ON factoid.id = factoid_id
+    )
+    WHERE is_approved
+    GROUP BY id         
+    `)
+    let unfilteredFacts = getFactsStmt.all()
+    unfilteredFacts.forEach(fact => {
+        fact.taglist = fact.taglist ? fact.taglist.split(',').sort() : []
+    })
+
+    let filteredFacts = filterFacts(unfilteredFacts, tags)
+
+    return filteredFacts.map(fact => {
+        return getFactByID(fact.id)
+    })
+}
+console.log(getFacts())
+
+function filterFacts(facts, tags = []) {
+    tags = !Array.isArray(tags) ? [tags] : tags
+    return facts.filter(fact => {
+        return tags.every(tag => {
+            return fact.taglist.includes(tag)
+        })
+    })
+}
