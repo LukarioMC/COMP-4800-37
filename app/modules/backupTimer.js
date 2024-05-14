f22c4f4c9f7f6976a015be5c568745ce92d75faf
AlexSichitiu <asichitiu@gmail.com>
2024-05-14 13:45:41 -0700

    Documented backupTimer module and refactored constant names.

--- app/modules/backupTimer.js
+++ app/modules/backupTimer.js
@@ -2,17 +2,23 @@ const db = require('better-sqlite3')('app.db');
 const fs = require('fs')
 
 const BACKUP_INTERVAL = 1000 * 60 * 60 * 24
-const maxBackups = 100
-const backUpDirName = 'db_backups'
+const MAX_BACKUPS = 100
+const BACKUP_DIR_NAME = 'db_backups'
 
+/**
+ * Create the backup directory if it does not exist.
+ */
 function init() {
-    if (!fs.existsSync(backUpDirName)){
-        fs.mkdirSync(backUpDirName);
+    if (!fs.existsSync(BACKUP_DIR_NAME)){
+        fs.mkdirSync(BACKUP_DIR_NAME);
     }
 }
 
+/**
+ * Save a back up of the database in the backup directory.
+ */
 function backupDB() {
-    db.backup(`${backUpDirName}/backup-${Date.now()}.db`)
+    db.backup(`${BACKUP_DIR_NAME}/backup-${Date.now()}.db`)
   .then(() => {
     console.log('backup complete!');
     trimBackups()
@@ -22,24 +28,39 @@ function backupDB() {
   });
 }
 
+/**
+ * If there are more than MAX_BACKUPS backup files in the backup dir, delete the oldest ones until there are equal to MAX_BACKUP
+ */
 function trimBackups() {
-    let backupDir = fs.readdirSync(`./${backUpDirName}`) 
-    while (backupDir.length > maxBackups) { 
+    let backupDir = fs.readdirSync(`./${BACKUP_DIR_NAME}`) 
+    while (backupDir.length > MAX_BACKUPS) { 
         let oldestBackup = backupDir.sort()[0]
-        fs.unlinkSync(`${backUpDirName}/${oldestBackup}`)
-        backupDir = fs.readdirSync(`./${backUpDirName}`)
+        fs.unlinkSync(`${BACKUP_DIR_NAME}/${oldestBackup}`)
+        backupDir = fs.readdirSync(`./${BACKUP_DIR_NAME}`)
     }
 }
 
+/** Class that controls the backup interval. */
 class BackupTimer {
+
+
+    /**
+     * Create a backup timer.
+     */
     constructor() { init() }
 
+    /**
+     * Start the backup timer.
+     */
     start() {
         this.timer = setInterval(() => {
             backupDB()
         }, BACKUP_INTERVAL) 
     }
 
+    /**
+     * Stop the backup timer.
+     */
     stop() { clearInterval(timer) }
 }
 
