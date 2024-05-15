7e49b3ba36204d81d13c1be2c31b7ff401065594
Liana Diu <liana.diu@hotmail.com>
2024-05-15 13:55:35 -0700

    added handling for case where there are no attachments

--- app/handlers/attachment.js
+++ app/handlers/attachment.js
@@ -2,24 +2,37 @@ const db = require('better-sqlite3')('app.db');
 
 function deleteAttachmentforFactoid(attachmentID){
     try {
-        const result = db.run('DELETE FROM Attachment WHERE id = ?', attachmentID);
+        const deleteAttachmentStatement = db.prepare('DELETE FROM Attachment WHERE id = ?');
+        const result = deleteAttachmentStatement.run(attachmentID);
         return result.changes > 0;
     } catch (e) {
-        console.error('Error deleting attachment:', e);
+        console.log('Error deleting attachment:', e);
         return false;
     }
 }
 
-function deleteAllAttachmentsforFactoid(attachmentID){
+function deleteAllAttachmentsforFactoid(factoidID) {
     try {
-        const attachments = db.prepare('SELECT * FROM Attachment WHERE id = ?').all(attachmentID)
-    
-        attachments.forEach(attachment => {
-            deleteAttachmentforFactoid(attachment.ID);
+        const getAllAttachmentsStatement = db.prepare('SELECT * FROM Attachment WHERE factoid_id = ?')
+            
+        const allAttachmentsForFact = getAllAttachmentsStatement.all(factoidID);
+     
+        if (allAttachmentsForFact.length === 0) {
+            console.log("No attachments found for the factoid.");
+            return true; // Return true as there are no attachments to delete
+        }
+        
+        allAttachmentsForFact.forEach(attachment => {
+            const result = deleteAttachmentforFactoid(attachment.id);
+            if (!result) {
+                throw new Error(`Error deleting attachment with ID ${attachment.id}`);
+            }
         });
 
-    } catch (e) {
-
+        return true; // Return true if all attachments are deleted successfully
+    } catch (error) {
+        console.error('Error deleting attachments for the factoid:', error.message);
+        return false; 
     }
 }
 
