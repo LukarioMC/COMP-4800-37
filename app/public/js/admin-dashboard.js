750fc120b1a4d3b660b0e3d208343983844b4bea
Liana Diu <liana.diu@hotmail.com>
2024-05-24 11:50:20 -0700

    added function stubs for admin dashboard features

---
+++ app/public/js/admin-dashboard.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+});
+
+async function deleteFact(factID) {
+}
+
+async function approveFact(factID) {
+}
+
+function editFact(factID) {
+}
