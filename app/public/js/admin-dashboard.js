471fc913479cfbd6919698b6eeb296a5df466727
Liana Diu <liana.diu@hotmail.com>
2024-05-24 11:53:18 -0700

    retrieve fact id

--- app/public/js/admin-dashboard.js
+++ app/public/js/admin-dashboard.js
@@ -2,14 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('dashboard').addEventListener('click', async function(event) {
         const target = event.target;
         if (target.classList.contains('action-btn')) {
+            const factID = target.closest('.row').dataset.id;
             const action = target.dataset.action;
 
             if (action === 'approve') {
-                await approveFact();
+                await approveFact(factID);
             } else if (action === 'delete') {
-                await deleteFact();
+                await deleteFact(factID);
             } else if (action === 'edit') {
-                editFact();
+                editFact(factID);
             }
         }
     });
