853bc0f23c6669ffd37cc226f7c8cd735e0f56ea
Liana Diu <liana.diu@hotmail.com>
2024-05-24 14:20:54 -0700

    on document load, add the initially selected tags to the Set

--- app/public/js/edit-factoid.js
+++ app/public/js/edit-factoid.js
@@ -2,5 +2,10 @@
 const selectedTags = new Set();
 
 document.addEventListener('DOMContentLoaded', function() {
-    
+    // if a tag is selected, then add it to the selectedTags Set
+    document.querySelectorAll('.tag-selectable').forEach(tagElement => {
+        if (tagElement.classList.contains('bg-primary')) {
+            selectedTags.add(tagElement.getAttribute('data-tag'));
+        }
+    });
 })
