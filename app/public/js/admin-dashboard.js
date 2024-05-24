e384d14179f02909bae97a816bd694d7438fcd3d
Liana Diu <liana.diu@hotmail.com>
2024-05-24 11:52:54 -0700

    link function to buttons

--- app/public/js/admin-dashboard.js
+++ app/public/js/admin-dashboard.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('dashboard').addEventListener('click', async function(event) {
+        const target = event.target;
+        if (target.classList.contains('action-btn')) {
+            const action = target.dataset.action;
+
+            if (action === 'approve') {
+                await approveFact();
+            } else if (action === 'delete') {
+                await deleteFact();
+            } else if (action === 'edit') {
+                editFact();
+            }
+        }
+    });
 });
 
 async function deleteFact(factID) {
