8d95c02bf8c50a0084a92565a14ad3d117d44634
Dakaro Mueller <LukarioMC@gmail.com>
2024-05-27 12:54:32 -0700

    Refactored to not refresh page on successful report resolution

--- app/public/js/adminReport.js
+++ app/public/js/adminReport.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('reportsDashboard').addEventListener('click', async function(event) {
-        const target = event.target;
-        if (target.classList.contains('resolve-btn')) {
-            const reportID = target.name;
-            deleteReport(reportID, target);
-        }
-    }) 
+    const resolveBtns = document.querySelectorAll('.resolve-btn');
+    for (btn of resolveBtns) {
+        btn.addEventListener('click', (event) => {
+            const target = event.target;
+            if (!target) return;
+            const reportID = target.dataset.report;
+            const reportElem = document.getElementById(`report-${reportID}`);
+            deleteReport(reportID, reportElem);
+        }) 
+    }
 })
 
 /**
  * A fetch request to the delete route for /api/report/:reportID
- * 
  * @param {number} reportID The ID of the report 
  */
-async function deleteReport(reportID, target) {
-    const response = await fetch(`/api/report/${reportID}`, {
+async function deleteReport(reportID, reportElement) {
+    await fetch(`/api/report/${reportID}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(() => {
-        window.location.reload();
-    }
-    )  
+    if (reportElement instanceof Element) reportElement.remove();
 }
