a2ded7e314a77dd59c4a6255a72bf1f255ca76f8
Dakaro Mueller <LukarioMC@gmail.com>
2024-05-28 02:11:43 -0700

    Change badge color as well

--- app/public/js/factoids.js
+++ app/public/js/factoids.js
@@ -17,7 +17,7 @@ function createSearchTag(name) {
     tag.className = "badge rounded-pill bg-primary me-2"
     tag.id = name
     tag.innerHTML = name + `
-    <button type="button" class="btn-close" aria-label="Close"></button>
+    <button type="button" class="btn-close btn-close-white" aria-label="Remove Tag"></button>
     `
     document.getElementById('tags').appendChild(tag)
     tag.onclick = () => {
