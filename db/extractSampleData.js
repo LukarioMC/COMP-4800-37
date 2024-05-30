8983d7ccbfae22d23e6e759f8cff0d322001ef38
Dakaro Mueller <LukarioMC@gmail.com>
2024-05-29 23:52:22 -0700

    Added basic insert script creation, changed URLs to object with category names, removed function for brevity

--- db/extractSampleData.js
+++ db/extractSampleData.js
@@ -5,28 +5,29 @@
 
 const fs = require('fs');
 const { JSDOM } = require('jsdom');
-const { document, Node, HTMLHeadingElement } = (new JSDOM(`...`)).window;
+const { document, Node } = (new JSDOM(`...`)).window;
+const { inferType, parseYoutubeUrlToEmbeded } = require('../app/modules/upload');
 
 const OUTPUT_FILE = './extractedSampleData.sql';
 const BASE_URL = 'http://thirty-seven.org/';
-const URLS = [
-	'http://thirty-seven.org/amazing.html',
-	'http://thirty-seven.org/historical.html',
-	'http://thirty-seven.org/ephemeral.html',
-	'http://thirty-seven.org/random.html',
-	'http://thirty-seven.org/sports.html',
-	'http://thirty-seven.org/scientific.html',
-	'http://thirty-seven.org/personal.html',
-	'http://thirty-seven.org/numerical.html',
-	'http://thirty-seven.org/movies.html',
-	'http://thirty-seven.org/comics.html',
-	'http://thirty-seven.org/othermedia.html',
-	'http://thirty-seven.org/37thones.html',
-	'http://thirty-seven.org/pictures.html',
-	'http://thirty-seven.org/sounds.html',
-	// 'http://thirty-seven.org/links.html',
-	'http://thirty-seven.org/multiples.html'
-];
+const URLS = {
+	'Amazing': 'http://thirty-seven.org/amazing.html',
+	'Historical': 'http://thirty-seven.org/historical.html',
+	'Ephemeral': 'http://thirty-seven.org/ephemeral.html',
+	'Random': 'http://thirty-seven.org/random.html',
+	'Sports': 'http://thirty-seven.org/sports.html',
+	'Scientific': 'http://thirty-seven.org/scientific.html',
+	'Personal': 'http://thirty-seven.org/personal.html',
+	'Numerical': 'http://thirty-seven.org/numerical.html',
+	'Movies': 'http://thirty-seven.org/movies.html',
+	'Comics': 'http://thirty-seven.org/comics.html',
+	'Media': 'http://thirty-seven.org/othermedia.html',
+	'37th Things': 'http://thirty-seven.org/37thones.html',
+	'Pictures': 'http://thirty-seven.org/pictures.html',
+	'Sounds': 'http://thirty-seven.org/sounds.html',
+	// 'Links': 'http://thirty-seven.org/links.html',
+	'Multiples': 'http://thirty-seven.org/multiples.html'
+};
 
 /**
  * Fetches a HTML document and returns the body of the document.
@@ -49,35 +50,20 @@ async function fetchHTMLDocument(url) {
 async function fetchAndParseData(links) {
 	const factoids = [];
 	for (const link of links) {
-		const items = await fetchListItems(link);
-		for (const item of items) {
-			const { rawFactoid, note } = await getNote(item); // Fetch note data
-			const { factoid, attachments } = parsefactoid(rawFactoid);
-			factoids.push({ factoid, note, attachments });
+		try {
+			const body = await fetchHTMLDocument(link);
+			// Get all list items from the parsed document
+			const listItems = body.querySelectorAll('ul li, ol li');
+			for (const item of Array.from(listItems)) {
+				const { rawFactoid, note } = await getNote(item); // Fetch note data
+				const { factoid, attachments } = parsefactoid(rawFactoid);
+				factoids.push({ factoid, note, attachments });
+			}
+		} catch (error) {
+			console.error(`Error fetching data for url ${link}`, error);
 		}
 	}
 	return factoids;
-}
-
-/**
- * Given a URL that returns a HTML document, fetch it's contents and return the
- * items on the page as an array.
- * 
- * Example usage:
- * fetchListItems('https://example.com/page-with-list').then(array => console.log(array));
- * @param {string} url to fetch from
- * @returns HTMLElement[] of unparsed list items.
- */
-async function fetchListItems(url) {
-  try {
-    const body = await fetchHTMLDocument(url);
-		// Get all list items from the parsed document
-    const listItems = body.querySelectorAll('ul li, ol li');
-    return Array.from(listItems);
-  } catch (error) {
-    console.error('Error fetching the list items:', error);
-    return [];
-  }
 }
 
 /**
@@ -168,5 +154,73 @@ function parsefactoid(listItem) {
   return { factoid, attachments };
 }
 
-// fetchAndParseData(URLS);
-fetchAndParseData(['http://thirty-seven.org/multiples.html']);
+/**
+ * Appends tag insert statements to the end of the passed in `statements`. Sets
+ * each `key` to be a primary tag.
+ * 
+ * @param {string[]} keys to be used as category names
+ * @param {string[]} statements SQL statements to be joined later.
+ * @returns Object of category names to inserted category ID.
+ */
+function insertCategories(keys, statements) {
+	let id = 373737; // Starting category id
+	let categories = {};
+	// Create OG category
+	statements.push(`INSERT INTO Category (id, name, is_primary) VALUES (${id++}, 'OG', TRUE);`);
+	// Create rest of the categories
+	for(const key in keys) {
+		categories[key] = id;
+		statements.push(`INSERT INTO Category (id, name, is_primary) VALUES (${id++}, ${key}, TRUE);`);
+	}
+	return categories;
+}
+
+/**
+ * Creates a SQL file at the configured ouput location. 
+ * @param {Object[]} factoids 
+ */
+function createSQLFile(factoids) {
+	const data = [];
+	let id = 373737; // Starting Factoid ID
+	// Returns {string: int} for fact ID lookup after creating insertion statements
+	const categories = insertCategories(Object.keys(URLS), data);
+	for (const { factoid, note, attachments, category } of factoids) {
+		if (!factoid) throw new Error('Invalid factoid data provided!');
+		data.push(`-- INSERTING DATA FOR FACT ID ${id}`);
+		data.push(
+			`INSERT INTO Factoid (id, submitter_id, content, posting_date, discovery_date, note, is_approved, approval_date)
+			VALUES (
+				${id},
+				'mag3737',
+				${factoid},
+				datetime(1997-02-06 00:22:49),
+				datetime(1997-02-06 00:22:49),
+				${note},
+				TRUE,
+				CURRENT_TIMESTAMP
+			);`
+		);
+		// Create category tag entry
+		data.push(`INSERT INTO Tag (factoid_id, category_id) VALUES (${id}, ${categories[category]});`);
+		// Insert the factoids attachments and parse it if it's a youtube link.
+		for (const url of attachments) {
+			const type = inferType(url);
+			if (type === 'youtube') {
+				url = parseYoutubeUrlToEmbeded(url);
+			}
+			data.push(
+				`INSERT INTO Attachment (factoid_id, link, type)
+				VALUES (
+					${id},
+					${url},
+					${type}
+				);`
+			);
+		}
+	}
+	// Make the insert SQL file.
+	fs.writeFileSync(OUTPUT_FILE, data.join('\n'));
+}
+
+fetchAndParseData(Object.values(URLS))
+.then(createSQLFile);
